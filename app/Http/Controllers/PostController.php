7875f166c718893506af56dd5cdf5e84d4486e7a
SadekAlshaar018 <Your Actual Email, Sadek>
2017-12-22 18:39:34 +0100

    Delete
    destroy method is klaar

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -113,6 +113,8 @@ class PostController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $post = Post::find($id);
+        $post->delete();
+        return redirect('/posts')->with('success', 'post deleted');
     }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,6 +3,11 @@
 @section('content')
 <a href="http://localhost:8889/lsapp/public/posts" class="btn btn-default">Go Back</a>
 <a href="http://localhost:8889/lsapp/public/posts/{{$post->id}}/edit" class="btn btn-default"> Edite</a>
+  {{-- form for edit post and function  --}}
+  {!! Form::open(['action' => ['PostController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right']) !!}
+    {{form::hidden('_method', 'DELETE')}}
+    {{form::submit('Delete', ['class' => 'btn btn-danger'])}}
+  {!!Form::close()!!}
   <h1>{{$post->title}}</h1>
   <div class="">
     {{-- i used this lik {!!$post->body!!} cuz a source in editor body --}}
